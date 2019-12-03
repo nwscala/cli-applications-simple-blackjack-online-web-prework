1954484f25a3fdd8bf93107165e5be29f14a441b
Noah Scala <tobi8912@gmail.com>
2019-12-03 21:49:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,5 +64,19 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  prompt_user
+  get_user_input
+  if get_user_input == "s"
+    prompt_user
+    get_user_input
+  elsif get_user_input == "h"
+    card_total += deal_card
+    display_card_total(card_total)
+  end 
+  if card_total > 21
+    end_game
+  end
 end
     
